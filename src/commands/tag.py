7bc723c669f58133697e48025d1c4b8cddaf4f77
Róisín Grannell <r.grannell2@gmail.com>
2023-10-31 17:13:36 +0000

    now indexes images after tagging them

--- src/commands/tag.py
+++ src/commands/tag.py
@@ -1,6 +1,7 @@
 
 from src.photo import PhotoVault, Photo
 from src.tags import TagMetadata
+from src.manifest import Manifest
 
 def tag(dir: str, metadata_path: str):
   """Read tags.md files in each photo-directory, and write extended
@@ -15,3 +16,16 @@ def tag(dir: str, metadata_path: str):
     album = entry['album']
 
     Photo(fpath).set_metadata(attrs, album, tag_metadata)
+
+  db = Manifest()
+
+  for image in PhotoVault(dir).list_images():
+    db.add_image(image)
+
+  for album in PhotoVault(dir).list_albums():
+    md = album.get_metadata()
+
+    if not md:
+      continue
+
+    db.add_album(md)
