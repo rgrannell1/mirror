fa722677e28fb361ddaf943a4883e90b767bfeae
Róisín Grannell <r.grannell2@gmail.com>
2024-12-27 13:47:27 +0000

    moved commands to dedicated folder

--- cli.py
+++ cli.py
@@ -1,11 +1,11 @@
 import sys
 from src.ansi import ANSI
-from src.artifacts import ArtifactBuilder
+from src.commands.publisher import ArtifactBuilder
 from src.cdn import CDN
 from src.config import DATABASE_PATH, OUTPUT_DIRECTORY, PHOTO_DIRECTORY
 from src.database import SqliteDatabase
-from src.uploader import MediaUploader
-from src.scanner import MediaScanner, LinnaeusScanner
+from src.commands.uploader import MediaUploader
+from src.commands.scanner import MediaScanner, LinnaeusScanner
 
 commands = ["mirror scan", "mirror upload", "mirror publish"]
 
